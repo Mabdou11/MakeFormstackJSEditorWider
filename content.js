64da1d09313394bf2693f61da6f4802d3a714e2f
Abdou Menouer <menouerabdou@gmail.com>
2021-04-23 03:30:57 +0100

    wait for the editor to exist

--- content.js
+++ content.js
@@ -1,2 +1,7 @@
-document.getElementById("javascriptEditor").style.width="100%";
-document.getElementById("javascriptEditor").parentNode.style.width="200%";
+import { elementReady } from "es6-element-ready";
+
+const changeWidth = () => {
+	document.getElementById("javascriptEditor").style.width="100%";
+	document.getElementById("javascriptEditor").parentNode.style.width="200%";
+}
+elementReady('#javascriptEditor').then(changeWidth());
